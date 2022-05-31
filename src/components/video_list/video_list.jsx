afc8c09b1f74aa151f800fd1d10e7baeb0875d72
YunSukHyun <czcz9207@gmail.com>
2022-06-01 02:37:09 +0900

    make video item

--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import Video_item from '../video_item/video_item';
-
-const Video_list = (props) => (
-  <ul>
-    {props.videos.map(video => (<Video_item key={video.id} video={video}/>))}
+import VideoItem from '../video_item/video_item';
+import styles from './video_list.module.css';
+const VideoList = (props) => (
+  <ul className={styles.videos}>
+    {props.videos.map(video => (
+      <VideoItem key={video.id} video={video}/>))}
   </ul>
 );
 
-export default Video_list;
+export default VideoList;
